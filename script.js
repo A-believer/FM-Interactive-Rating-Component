68990af8f9bead3ddd049e95e885e86836ac2f3a
davedev <davidabolade29@gmail.com>
2023-01-10 21:36:29 +0100

    completed

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const options = document.getElementsByTagName('li');
-const select = document.getElementById('select')
 
-console.log(options.textContent);
-console.log(select)
+const ratingPage = document.querySelector("#rating-page")
+const thankYouPage = document.querySelector("#thank-you-page")
+const submitBtn = document.querySelector(".submit-btn")
+const selectedRate = document.querySelector("#selected")
+let rate = 0;
+const ratingList = document.querySelectorAll(".rate")
 
- let newOptions = Array.from(options);
+ratingList.forEach(rated => {
+    rated.addEventListener("click", function () {
+        rate = this.innerHTML;
+        ratingList.forEach(btn => btn.classList.remove("active"));
+        this.classList.add('active');
+    })
+})
 
-console.log(newOptions);
+submitBtn.addEventListener("click", rateHandler)
+
+function rateHandler() {
+    if (rate === 0) {
+        alert("Please rate us!")
+    } else {
+        selectedRate.innerHTML = rate;
+        ratingPage.style.display = "none"
+        thankYouPage.style.display = "block"
+    }
+}
